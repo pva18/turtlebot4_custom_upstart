5a6726f31f267d5138f9423b57ddee64d5b9e89a
Péter Varga <petervarga0018@gmail.com>
2024-12-18 13:44:10 +0100

    Change path resolution in install and extend docs

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # turtlebot4_custom_upstart
+
 This package provides a custom upstart configuration for the turtlebot4 robot.
 
-To install the configuration, use the `install.py` script. Before usage use the `turtlebot4_setup` package to setup the robot with the necessary environment variables. After the installation process in `install.py`, reinstall turtlebot4 service with turtlebot4-setup.
+To install the configuration, use the `scripts/install.py` script. Before usage, use the `turtlebot4_setup` package to setup the robot with the necessary environment variables. After the installation process in `scripts/install.py`, reinstall turtlebot4 service with turtlebot4-setup.
+(turtlebot4-setup -> ROS Setup -> Upstart -> Uninstall -> Install -> Start)
+
+**Note:**
+*!NEW VERSION! See TODO below!*
+The install script uses absolute paths for configuration files. For this reason, it is required for this package to be installed to "/home/ubuntu/ros2_ws/src/turtlebot4_custom_upstart".
+If this packages is installed to another place you need to modify the install script.
+
+**TODO:**
+Test the new install script with paths resolved by `ament_index_python.packages.get_package_share_directory`.
--- scripts/install.py
+++ scripts/install.py
@@ -2,6 +2,9 @@ import os
 import subprocess, shlex
 
 # TODO: Do not use absolute paths for the ros2_ws paths.
+# TODO: Test the correct working of paths
+from ament_index_python.packages import get_package_share_directory
+
 
 def main():
     """
@@ -10,29 +13,59 @@ def main():
     This script assumes that the ROS 2 workspace is located at /home/ubuntu/ros2_ws and was built using the colcon build command after downloading the turtlebot4_custom_upstart package.
     """
 
+    print("\n--------\nCHECK TODO!\n-------\n")
+
+    turtlebot4_bringup_dir = get_package_share_directory("turtlebot4_bringup")
+    turtlebot4_bringup_launch_dir = os.path.join(turtlebot4_bringup_dir, "launch")
+    turtlebot4_custom_upstart_dir = get_package_share_directory("turtlebot4_custom_upstart")
+    turtlebot4_custom_upstart_launch_dir = os.path.join(turtlebot4_custom_upstart_dir, "launch")
+
     # Back up the original launch file
-    if not os.path.exists("/opt/ros/humble/share/turtlebot4_bringup/launch/standard.launch.py.bak"):
+    # if not os.path.exists("/opt/ros/humble/share/turtlebot4_bringup/launch/standard.launch.py.bak"):
+    #     subprocess.run(
+    #         shlex.split(
+    #             "sudo mv "
+    #             "/opt/ros/humble/share/turtlebot4_bringup/launch/standard.launch.py "
+    #             "/opt/ros/humble/share/turtlebot4_bringup/launch/standard.launch.py.bak"
+    #         )
+    #     )
+    if not os.path.exists(os.path.join(turtlebot4_bringup_launch_dir, "standard.launch.py.bak")):
         subprocess.run(
             shlex.split(
-                "sudo mv "
-                "/opt/ros/humble/share/turtlebot4_bringup/launch/standard.launch.py "
-                "/opt/ros/humble/share/turtlebot4_bringup/launch/standard.launch.py.bak"
+                " ".join(
+                    [
+                        "sudo mv",
+                        os.path.join(turtlebot4_bringup_launch_dir, "standard.launch.py"),
+                        os.path.join(turtlebot4_bringup_launch_dir, "standard.launch.py.bak"),
+                    ]
+                )
             )
         )
-    print(
-        "Backed up the original launch file (turtlebot4_bringup/launch/standard.launch.py)."
-    )
+    print("Backed up the original launch file (turtlebot4_bringup/launch/standard.launch.py).")
     # Copy the custom launch file
+    # subprocess.run(
+    #     shlex.split(
+    #         "sudo cp "
+    #         "/home/ubuntu/ros2_ws/src/turtlebot4_custom_upstart/launch/robot_upstart.launch.py "
+    #         "/opt/ros/humble/share/turtlebot4_bringup/launch/standard.launch.py"
+    #     )
+    # )
     subprocess.run(
         shlex.split(
-            "sudo cp "
-            "/home/ubuntu/ros2_ws/src/turtlebot4_custom_upstart/launch/robot_upstart.launch.py "
-            "/opt/ros/humble/share/turtlebot4_bringup/launch/standard.launch.py"
+            " ".join(
+                [
+                    "sudo cp",
+                    os.path.join(turtlebot4_custom_upstart_launch_dir, "robot_upstart.launch.py"),
+                    os.path.join(turtlebot4_bringup_launch_dir, "standard.launch.py"),
+                ]
+            )
         )
     )
     print(
         "Copied the custom launch file (turtlebot4_custom_upstart/launch/robot_upstart.launch.py) to turtlebot4_bringup launch directory."
     )
+
+    workspace_setup_path = os.path.join(turtlebot4_custom_upstart_launch_dir, "../../../setup.bash")
 
     # Change the WORKSPACE_SETUP variable in the robot setup script file
     robot_setup_file = os.environ["ROBOT_SETUP"]
@@ -41,16 +74,13 @@ def main():
     with open(robot_setup_file, "w") as f:
         for line in lines:
             if "export WORKSPACE_SETUP" in line:
-                f.write(
-                    'export WORKSPACE_SETUP="/home/ubuntu/ros2_ws/install/setup.bash"\n'
-                )
+                # f.write('export WORKSPACE_SETUP="/home/ubuntu/ros2_ws/install/setup.bash"\n')
+                f.write(f'export WORKSPACE_SETUP="{workspace_setup_path}"\n')
             else:
                 f.write(line)
     print("Changed the WORKSPACE_SETUP variable in the robot setup script file.")
 
-    print(
-        "To apply the changes, reinstall the robot upstart service using the turtlebot4-setup tool."
-    )
+    print("To apply the changes, reinstall the robot upstart service using the turtlebot4-setup tool.")
 
 
 if __name__ == "__main__":
